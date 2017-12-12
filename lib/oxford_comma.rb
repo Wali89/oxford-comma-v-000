2d46ad4b816ff69185e2abf4d30c8df2dfe7185f
Wali Rahman <wali.rahman89@yahoo.com>
2017-12-12 14:03:15 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,15 @@
 def oxford_comma(array)
   array.insert(-2, 'and')
-  x = array.length - 2
-  y = 0
-  while y < x do
-    array[y] << ","
-    y += 1
+  if array.length > 1
+    x = array.length - 2
+    y = 0
+    while y < x do
+      array[y] << ","
+      y += 1
+    end
+    coma = array.join(" ")
+    coma
+  else
+    array
   end
-  coma = array.join(" ")
-  coma
 end
